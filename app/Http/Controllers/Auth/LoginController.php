3b99567bd2de147ce2048aa9fa08331e8258bd04
Maksim Rudenko <rudenko.programmer@gmail.com>
2018-04-29 02:48:13 +0300

    setup db connection & autorisation & social

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use App\SocialAccount;
+use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +30,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -35,5 +40,74 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+    {
+        $user = $this->createOrGetUser($provider);
+
+        /* Log in the user */
+        Auth::login( $user );
+
+        /* Redirect to the app */
+        return redirect('/');
+        
+    }
+
+
+    public function createOrGetUser($provider)
+    {
+        $providerUser = Socialite::driver($provider)->user();
+        $account = SocialAccount::whereProvider($provider)
+            ->whereProviderId($providerUser->getId())
+            ->first();          
+
+        if ($account) {
+            return $account->user;
+        } else {
+            
+            $user = User::whereEmail($providerUser->getEmail())->first();
+                        
+            if (!$user) {
+                $login = $providerUser->getNickname();
+                if($login == ''){
+                    $parts = explode("@", $providerUser->getEmail());
+                    $loginNative = $login = $parts[0];
+
+                    $counter = 1;
+                    while (User::whereLogin($login)->first()){
+                        $login = $loginNative."_".$counter;
+                        $counter++;
+                    }
+                }
+
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name' => $login,
+                    'password' => bcrypt(random_bytes(10)),
+                ]);
+            }
+
+            $account = new SocialAccount([
+                'user_id' => $user->id,
+                'provider_id' => $providerUser->getId(),
+                'provider' => $provider
+            ]);       
+
+            $account->save();
+
+            return $user;
+        }
+
     }
 }
--- config/services.php
+++ config/services.php
@@ -35,4 +35,33 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'github' => [
+        'client_id'     => env('GITHUB_ID'),
+        'client_secret' => env('GITHUB_SECRET'),
+        'redirect'      => env('GITHUB_URL'),
+    ],
+
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_ID'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect'      => env('FACEBOOK_URL'),
+    ],
+
+    'google' => [
+        'client_id'     => env('GOOGLE_ID'),
+        'client_secret' => env('GOOGLE_SECRET'),
+        'redirect'      => env('GOOGLE_URL'),
+    ],
+
+    'linkedin' => [
+        'client_id' => env('LINKEDIN_ID'),
+        'client_secret' => env('LINKEDIN_SECRET'),
+        'redirect' => env('LINKEDIN_URL'),
+    ],
+
+    'twitter' => [
+        'client_id'     => env('TW_ID'),
+        'client_secret' => env('TW_SECRET'),
+        'redirect'      => env('TW_URL')
+    ],
 ];
